5d728ed1abb8bcb03ffdfde6d35912cbe6f26ce0
LAPTOP-CSEFN12N\guohu <guohuatian777@163.com>
2022-08-18 21:40:18 +1200

    fie

--- app.js
+++ app.js
@@ -17,23 +17,27 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
+//app.get('/', (req, res) => res.send('Hello world!'));
 
 // use Routes
 app.use('/api/books', books);
-
-if(process.env.NODE_ENV  === "production"){
-    console.log(1);
-    app.use(express.static(path.join(__dirname, '/mern_a_to_z_client/build')))
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'mern_a_to_z_client', 'build', 'index.html')); 
-    });
-} else{
-    app.get('/',  (req, res) =>{
-        res.send("Api running");
-    });
-}
+app.use(express.static(path.join(__dirname, '/mern_a_to_z_client/build')))
+    
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'mern_a_to_z_client', 'build', 'index.html')); 
+});
+// if(process.env.NODE_ENV  === "production"){
+//     console.log(1);
+//     app.use(express.static(path.join(__dirname, '/mern_a_to_z_client/build')))
+    
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.join(__dirname, 'mern_a_to_z_client', 'build', 'index.html')); 
+// });
+// } else{
+//     app.get('/',  (req, res) =>{
+//         res.send("Api running");
+//     });
+// }
 
 const port = process.env.PORT || 5000; 
 
